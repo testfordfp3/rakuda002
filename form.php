5a460ff17685de7e2fc1fadae465f10ff76e340a
testfordfp3 <62692779+testfordfp3@users.noreply.github.com>
2022-11-02 18:05:32 +0900

    Add files via upload

---
+++ form.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>お問い合わせ｜株式会社GOLDENSEA JAPAN</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="GOLDENSEA JAPANは常に安全、安心、正確、迅速を心掛けて国際貿易関連の荷主様の立場に立ってきめ細かな物流サービスをモットーに輸出入通関申告、輸送、保管等のお手伝いをさせて頂いております。">
+<meta name="keywords" content="GOLDENSEA JAPAN,ゴールデンシージャパン,通関業務,国際物流,国際複合輸送,海上輸送,航空輸送,陸上運送,日中間,中国物流">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/slide.css">
+<link rel="stylesheet" href="css/fixmenu_pagetop.css"><link rel="icon" href="images/favicon.ico"/>
+<script type="text/javascript" src="js/openclose.js"></script>
+<script type="text/javascript" src="js/fixmenu_pagetop.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<div id="container">
+
+<header>
+<h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="goldensea japan"></a></h1>
+
+</header>
+
+<!--PC用（801px以上端末）メニュー-->
+<nav id="menubar">
+<ul>
+<li><a href="index.html">ホーム<span>HOME</span></a></li>
+<li><a href="company.html">会社概要<span>COMPANY</span></a></li>
+<li><a href="service.html">業務内容<span>SERVICE</span></a></li>
+<li><a href="recruit.html">採用情報<span>RECRUIT</span></a></li>
+<li><a href="link.html">リンク<span>LINK</span></a></li>
+<li class="current"><a href="form.php">お問い合わせ<span>CONTACT</span></a></li>
+</ul>
+</nav>
+<!--小さな端末用（800px以下端末）メニュー-->
+<nav id="menubar-s">
+<ul>
+<li><a href="index.html">ホーム<span>HOME</span></a></li>
+<li><a href="company.html">会社概要<span>COMPANY</span></a></li>
+<li><a href="service.html">業務内容<span>SERVICE</span></a></li>
+<li><a href="recruit.html">採用情報<span>RECRUIT</span></a></li>
+<li><a href="link.html">リンク<span>LINK</span></a></li>
+<li class="current"><a href="form.php">お問い合わせ<span>CONTACT</span></a></li>
+</ul>
+</nav>
+
+<div id="contents">
+
+<div id="main">
+
+<section>
+
+<h2>お問い合わせフォーム</h2>
+
+<!-- フォームの情報はここからです -->
+<form name="form1" enctype="multipart/form-data" method="post" action="/form.php">
+<input type="hidden" name="mode" value="form">
+<table class="ta1">
+<!-- 入力／確認 -->
+<!--<tr><th class="tamidashi" colspan="2">お問い合わせフォーム</th></tr>-->
+<tr>
+<th width="150">お名前<span style="color:red">※</span></th>
+<td>
+<input name="item1" value="" type="text" size="40" maxlength="500" class="ws">
+</td>
+</tr>
+<tr>
+<th width="150">メールアドレス<span style="color:red">※</span></th>
+<td>
+<input name="item2" value="" type="text" size="40" maxlength="500" class="ws">
+</td>
+</tr>
+<tr>
+<th>お問い合わせ内容<span style="color:red">※</span></th>
+<td>
+<textarea name="item3" cols="40" rows="10" class="wl"></textarea>
+</td>
+</tr>
+</table>
+<div class="c">
+<input type="submit" value="内容を確認する" class="btn">
+&nbsp;
+<input type="reset" value="リセット" class="btn">
+</div>
+</form>
+<!-- フォームの情報はここまでです -->
+
+</section>
+
+</div>
+<!--/main-->
+
+<div id="sub">
+<section class="box">
+<h2>Sub Menu</h2>
+
+<div class="list">
+<a href="index.html">
+<figure><img src="images/home_img.jpg" alt=""></figure>
+<h4>　ホーム</h4>
+<p>　HOME</p>
+</a>
+</div>
+
+<div class="list">
+<a href="company.html">
+<figure><img src="images/company_img.jpg" alt=""></figure>
+<h4>　会社概要</h4>
+<p>　COMPANY</p>
+</a>
+</div>
+
+<div class="list">
+<a href="service.html">
+<figure><img src="images/service_img.jpg" alt=""></figure>
+<h4>　業務内容</h4>
+<p>　SERVICE</p>
+</a>
+</div>
+
+<div class="list">
+<a href="recruit.html">
+<figure><img src="images/recruit_img.jpg" alt=""></figure>
+<h4>　採用情報</h4>
+<p>　RECRUIT</p>
+</a>
+</div>
+
+<div class="list">
+<a href="link.html">
+<figure><img src="images/link_img.jpg" alt=""></figure>
+<h4>　リンク</h4>
+<p>　LINK</p>
+</a>
+</div>
+
+<div class="list">
+<a href="form.php">
+<figure><img src="images/contact_img.jpg" alt=""></figure>
+<h4>　お問い合わせ</h4>
+<p>　CONTACT</p>
+</a>
+</div>
+
+</section>
+
+<section class="box">
+
+<h2>事績案件</h2>
+
+<div class="list">
+<a href="jisseki1.html">
+<figure><img src="images/j1.jpg" alt="プレス機"></figure>
+<h4><strong>　重量物の大型機械</strong></h4>
+<p>　150ｔ超え大型機械の分解輸送</p>
+</a>
+</div>
+
+<div class="list">
+<a href="#2">
+<figure><img src="images/sample1.jpg" alt="製品名"></figure>
+<h4>　事績案件</h4>
+<p>　工事中</p>
+</a>
+</div>
+
+<div class="list">
+<a href="#3">
+<figure><img src="images/sample1.jpg" alt="製品名"></figure>
+<h4>　事績案件</h4>
+<p>　工事中</p>
+</a>
+</div>
+
+<div class="list">
+<a href="#4">
+<figure><img src="images/sample1.jpg" alt="製品名"></figure>
+<h4>　事績案件</h4>
+<p>　工事中</p>
+</a>
+</div>
+</section>
+
+<section class="box">
+<h2>運営</h2>
+<p class="c"><strong>株式会社GOLDENSEA JAPAN</strong><br>
+<span class="mini1">東京都中央区日本橋蛎殻町1-2-3<br>
+ルート蛎殻町第二ビル2F<br>
+TEL:03-6667-0287<br>
+(AM9:00〜PM6:00 土日祝休み)</span></p>
+</section>
+
+</div>
+<!--/sub-->
+
+</div>
+<!--/contents-->
+
+<footer>
+<small>Copyright&copy; <a href="index.html">GOLDENSEA JAPAN CO.,LTD.</a> All Rights Reserved.</small>
+</footer>
+
+</div>
+<!--/container-->
+
+<!--「↑」ボタン-->
+<p class="nav-fix-pos-pagetop"><a href="#">↑</a></p>
+
+<!--メニュー開閉ボタン-->
+<div id="menubar_hdr" class="close"></div>
+<!--メニューの開閉処理条件設定　800px以下-->
+<script type="text/javascript">
+if (OCwindowWidth() <= 800) {
+	open_close("menubar_hdr", "menubar-s");
+}
+</script>
+</body>
+</html>
